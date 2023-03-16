d2c864ccfab147f3f39d213adf46ac69dfadaf38
sulaiman282 <spidy.twitch.tv@gmail.com>
2023-03-17 00:39:41 +0600

    created vendor form

--- components/Admin/Add_Vendor/CreateVendor.js
+++ components/Admin/Add_Vendor/CreateVendor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Formik } from "formik";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -8,13 +8,35 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-import PersonIcon from '@mui/icons-material/Person';
-import PhoneIcon from '@mui/icons-material/Phone';
-import VerifiedIcon from '@mui/icons-material/Verified';
+import PersonIcon from "@mui/icons-material/Person";
+import PhoneIcon from "@mui/icons-material/Phone";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import axios from "../../../utils/axios";
+
 export default function CreateVendor() {
   const [isLoading, setIsLoading] = useState(false);
+
+  const [country, setCountry] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/address/countries/`
+      );
+      setCountry(response.data);
+    }
+    fetchData();
+  }, []);
+
+  const getState = async (countryvalue) => {
+    console.log("select value", countryvalue);
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/address/countries/`
+    );
+  };
+
   return (
-    <div className="border-2 flex justify-center">
+    <div className="lg:py-10 flex justify-center">
       <div className="lg:w-1/2 w-full">
         <Formik
           enableReinitialize
@@ -33,9 +55,43 @@ export default function CreateVendor() {
           }}
           validate={(values) => {
             const errors = {};
-            if (!values.first_name) {
-              errors.first_name = "Please enter your first name.";
-            }
+            // if (!values.first_name) {
+            //   errors.first_name = "Please enter your first name.";
+            // }
+            // if (!values.last_name) {
+            //   errors.last_name = "Please enter your last name.";
+            // }
+            // if (!values.number) {
+            //   errors.number = "Please enter your number.";
+            // }
+            // if (!values.email) {
+            //   errors.email = "Required";
+            // } else if (
+            //   !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            // ) {
+            //   errors.email = "Invalid email address";
+            // }
+            // if (!values.trade_license_number) {
+            //   errors.trade_license_number = "Please enter your trade license.";
+            // }
+            // if (!values.street) {
+            //   errors.street = "Please enter street address.";
+            // }
+            // if (!values.city_id) {
+            //   errors.city_id = "Please select  city.";
+            // }
+            // if (!values.state_id) {
+            //   errors.state_id = "Please select  state.";
+            // }
+            // if (!values.country_id) {
+            //   errors.country_id = "Please select country.";
+            // }
+            // if (!values.area_id) {
+            //   errors.area_id = "Please select area.";
+            // }
+            // if (!values.type) {
+            //   errors.type = "Please select addresss type.";
+            // }
 
             return errors;
           }}
@@ -248,18 +304,24 @@ export default function CreateVendor() {
                   }}
                 />
                 <p className="text-xs ml-3 text-red-700 mt-1">
-                  {errors.trade_license_number && touched.trade_license_number && errors.trade_license_number}
+                  {errors.trade_license_number &&
+                    touched.trade_license_number &&
+                    errors.trade_license_number}
                 </p>
               </div>
 
               <div className="flex md:justify-between md:flex-row flex-col gap-4">
-                <FormControl fullWidth size="small"    InputProps={{
+                <FormControl
+                  fullWidth
+                  size="small"
+                  InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">
                         <EmailIcon className="text-2xl text-primary " />
                       </InputAdornment>
                     ),
-                  }}>
+                  }}
+                >
                   <InputLabel
                     id="demo-simple-select-label"
                     style={{ color: "#5B177B", fontWeight: "bold" }}
@@ -270,15 +332,19 @@ export default function CreateVendor() {
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
                     value={values.country_id}
-                
                     label="Country"
                     name="country_id"
-                    onChange={handleChange}
+                    onChange={(e) => {
+                      handleChange(e);
+                      getState(e.target.value);
+                    }}
                     onBlur={handleBlur}
                   >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                    {country?.map((data, index) => (
+                      <MenuItem value={data?.id} key={index}>
+                        {data?.name}
+                      </MenuItem>
+                    ))}
                   </Select>
                   <p className="text-xs ml-3 text-red-700 mt-1">
                     {errors.country_id &&
@@ -291,15 +357,14 @@ export default function CreateVendor() {
                     id="demo-simple-select-label"
                     style={{ color: "#5B177B", fontWeight: "bold" }}
                   >
-                    Country
+                    State
                   </InputLabel>
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={values.country_id}
-                    
-                    label="Country"
-                    name="country_id"
+                    value={values.state_id}
+                    label="State"
+                    name="state_id"
                     onChange={handleChange}
                     onBlur={handleBlur}
                   >
@@ -308,33 +373,34 @@ export default function CreateVendor() {
                     <MenuItem value={30}>Thirty</MenuItem>
                   </Select>
                   <p className="text-xs ml-3 text-red-700 mt-1">
-                    {errors.country_id &&
-                      touched.country_id &&
-                      errors.country_id}
+                    {errors.state_id && touched.state_id && errors.state_id}
                   </p>
                 </FormControl>
               </div>
               <div className="flex md:justify-between md:flex-row flex-col gap-4">
-                <FormControl fullWidth size="small"    InputProps={{
+                <FormControl
+                  fullWidth
+                  size="small"
+                  InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">
                         <EmailIcon className="text-2xl text-primary " />
                       </InputAdornment>
                     ),
-                  }}>
+                  }}
+                >
                   <InputLabel
                     id="demo-simple-select-label"
                     style={{ color: "#5B177B", fontWeight: "bold" }}
                   >
-                    Country
+                    City
                   </InputLabel>
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={values.country_id}
-                
-                    label="Country"
-                    name="country_id"
+                    value={values.city_id}
+                    label="City"
+                    name="city_id"
                     onChange={handleChange}
                     onBlur={handleBlur}
                   >
@@ -343,25 +409,22 @@ export default function CreateVendor() {
                     <MenuItem value={30}>Thirty</MenuItem>
                   </Select>
                   <p className="text-xs ml-3 text-red-700 mt-1">
-                    {errors.country_id &&
-                      touched.country_id &&
-                      errors.country_id}
+                    {errors.city_id && touched.city_id && errors.city_id}
                   </p>
-                </FormControl>{" "}
+                </FormControl>
                 <FormControl fullWidth size="small">
                   <InputLabel
                     id="demo-simple-select-label"
                     style={{ color: "#5B177B", fontWeight: "bold" }}
                   >
-                    Country
+                    Area
                   </InputLabel>
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={values.country_id}
-                    
-                    label="Country"
-                    name="country_id"
+                    value={values.area_id}
+                    label="Area"
+                    name="area_id"
                     onChange={handleChange}
                     onBlur={handleBlur}
                   >
@@ -370,82 +433,84 @@ export default function CreateVendor() {
                     <MenuItem value={30}>Thirty</MenuItem>
                   </Select>
                   <p className="text-xs ml-3 text-red-700 mt-1">
-                    {errors.country_id &&
-                      touched.country_id &&
-                      errors.country_id}
+                    {errors.area_id && touched.area_id && errors.area_id}
                   </p>
                 </FormControl>
               </div>
               <div className="flex md:justify-between md:flex-row flex-col gap-4">
-                <FormControl fullWidth size="small"    InputProps={{
+                <div className="w-full">
+                  <TextField
+                    id="input-with-icon-textfield"
+                    label="Street Address"
+                    size="small"
+                    variant="outlined"
+                    className="noring w-full "
+                    placeholder="Street Address"
+                    name="street"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.street}
+                    InputLabelProps={{
+                      style: { color: "#5B177B", fontWeight: "bold" },
+                    }}
+                    inputProps={{
+                      autoComplete: "off",
+
+                      form: {
+                        autoComplete: "off",
+                      },
+                      style: {
+                        height: "24px",
+                        background: "none",
+                      },
+                    }}
+                  />
+                  <p className="text-xs ml-3 text-red-700 mt-1">
+                    {errors.street && touched.street && errors.street}
+                  </p>
+                </div>
+                <FormControl
+                  fullWidth
+                  size="small"
+                  InputProps={{
                     startAdornment: (
                       <InputAdornment position="start">
                         <EmailIcon className="text-2xl text-primary " />
                       </InputAdornment>
                     ),
-                  }}>
+                  }}
+                >
                   <InputLabel
                     id="demo-simple-select-label"
                     style={{ color: "#5B177B", fontWeight: "bold" }}
                   >
-                    Country
+                    Type
                   </InputLabel>
                   <Select
                     labelId="demo-simple-select-label"
                     id="demo-simple-select"
-                    value={values.country_id}
-                
-                    label="Country"
-                    name="country_id"
+                    value={values.type}
+                    label="Type"
+                    name="type"
                     onChange={handleChange}
                     onBlur={handleBlur}
                   >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                    <MenuItem value="Home">Home</MenuItem>
+                    <MenuItem value="Office">Office</MenuItem>
                   </Select>
                   <p className="text-xs ml-3 text-red-700 mt-1">
-                    {errors.country_id &&
-                      touched.country_id &&
-                      errors.country_id}
+                    {errors.type && touched.type && errors.type}
                   </p>
                 </FormControl>{" "}
-                <FormControl fullWidth size="small">
-                  <InputLabel
-                    id="demo-simple-select-label"
-                    style={{ color: "#5B177B", fontWeight: "bold" }}
-                  >
-                    Country
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={values.country_id}
-                    
-                    label="Country"
-                    name="country_id"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  >
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                  </Select>
-                  <p className="text-xs ml-3 text-red-700 mt-1">
-                    {errors.country_id &&
-                      touched.country_id &&
-                      errors.country_id}
-                  </p>
-                </FormControl>
               </div>
-         
+
               <Button
                 variant="text"
                 type="submit"
                 disabled={isLoading}
                 className="mt-10 w-full capitalize px-4 py-3 text-lg text-white font-bold bg-primary hover:bg-primary2 hover:shadow-xl duration-300 "
               >
-                Sign in
+                Create Vendor
               </Button>
             </form>
           )}
--- pages/_app.js
+++ pages/_app.js
@@ -5,18 +5,56 @@ import Layout from "../components/Layout/Layout";
 import { StyledEngineProvider } from "@mui/material/styles";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-
 import "../styles/globals.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useState, useEffect } from "react";
-
 import store from "../rtk/store/store";
 import { Provider } from "react-redux";
+
+import { AppProps } from 'next/app';
+//custom console
+const customConsole = (() => {
+  const nativeConsole = console;
+  const noop = () => {};
+
+  const customConsole = {
+    assert: noop,
+    clear: noop,
+    count: noop,
+    debug: noop,
+    dir: noop,
+    dirxml: noop,
+    error: noop,
+    group: noop,
+    groupCollapsed: noop,
+    groupEnd: noop,
+    info: noop,
+    log: noop,
+    markTimeline: noop,
+    memory: noop,
+    profile: noop,
+    profileEnd: noop,
+    table: noop,
+    time: noop,
+    timeEnd: noop,
+    timeStamp: noop,
+    timeline: noop,
+    timelineEnd: noop,
+    trace: noop,
+    warn: noop,
+  };
+
+  return customConsole;
+})();
+
 
 function MyApp({ Component, pageProps, router }) {
   const [loading, setLoading] = useState(true);
 
+  // useEffect(() => {
+  //   console = customConsole;
+  // }, []);
   useEffect(() => {
     setLoading(false);
   }, []);
@@ -41,44 +79,40 @@ function MyApp({ Component, pageProps, router }) {
         main: "#371B5C",
       },
       error: {
-        main: '#ff0000' // set the error color to red
-      }
+        main: "#ff0000", // set the error color to red
+      },
     },
     // add other theme properties as needed
   });
-
-
-
-
 
   return (
     <>
       <StyledEngineProvider injectFirst>
         {!loading && (
-           <Provider store={store}>
-                    <Layout>
-          <>
-            <ThemeProvider theme={theme}>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </ThemeProvider>
-            <ToastContainer
-              autoClose={3500}
-              hideProgressBar={true}
-              closeOnClick
-              pauseOnHover
-              draggable={true}
-              position="top-right"
-              toastClassName=""
-              bodyClassName=""
-              progressClassName=""
-              pauseOnFocusLoss={true}
-              newestOnTop={true}
-              theme="colored"
-            />
-          </>
-          </Layout>
-           </Provider>
+          <Provider store={store}>
+            <Layout>
+              <>
+                <ThemeProvider theme={theme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+                <ToastContainer
+                  autoClose={3500}
+                  hideProgressBar={true}
+                  closeOnClick
+                  pauseOnHover
+                  draggable={true}
+                  position="top-right"
+                  toastClassName=""
+                  bodyClassName=""
+                  progressClassName=""
+                  pauseOnFocusLoss={true}
+                  newestOnTop={true}
+                  theme="colored"
+                />
+              </>
+            </Layout>
+          </Provider>
         )}
       </StyledEngineProvider>
     </>
